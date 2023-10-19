941c6e5de070ccf60058b6e090c5fa62582750a0
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-10-19 13:57:54 -0700

    Update styling for global styles

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Beff Jezos';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'techno-apathy';
 
 export default function Layout({ children, home }) {
   return (
